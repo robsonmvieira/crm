bdb93e03de2dd5f322b27595085229c9ccc258ae
Robson Maia <robsonmvieira@gmail.com>
2024-05-05 21:34:36 +0100

    refactor(continuos integrations): :green_heart: fix prettier config file name

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
+  distDir: 'build',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
         // port: '',
-        pathname: '**',
+        pathname: '**'
       },
       {
         protocol: 'https',
         hostname: 'cdn.pixabay.com',
         // port: '',
-        pathname: '**',
+        pathname: '**'
       },
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
         // port: '',
-        pathname: '**',
-      },
+        pathname: '**'
+      }
     ]
     // domains: ["images.unsplash.com", "cdn.pixabay.com", "images.pexel.com"],
-  },
-};
+  }
+}
 
-export default nextConfig;
+export default nextConfig
