489d93e21c4c23c813251df55154bbd2a181b692
Robson Maia <robsonmvieira@gmail.com>
2024-05-05 12:19:41 +0100

    test(tests): :white_check_mark: update tests setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,20 @@ import { alias } from './alias'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{spec,test}.{js,ts,jsx,tsx}'], // Especifica onde os arquivos de teste estão localizados
+    coverage: {
+      provider: 'istanbul', // ou 'istanbul', dependendo do que você preferir
+      include: ['src/**/*.{ts,jsx,tsx}'], // Inclui apenas arquivos dentro de src para cobertura
+      exclude: [
+        'src/**/components/ui/**/*.{ts,jsx,tsx}',
+        'src/**/layout.tsx',
+        'src/lib/*',
+        'src/**/*.e2e-spec.ts'
+      ], // Inclui apenas
+      reporter: ['text', ['html', { subdir: 'coverage' }]]
+    }
   },
   resolve: {
     alias
